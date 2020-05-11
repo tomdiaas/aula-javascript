dcd160475f947787d4e0250e547e18c73821bf17
unknown <wto182@gmail.com>
2020-05-11 17:12:14 -0300

    primeiras funcoes com javascript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse"
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui"
+}
+
+function load(elemento){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
